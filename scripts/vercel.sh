ce2062c225d4cc25cf3aefe3a6ca14f8739f0d69
Reaper <ahoy@barelyhuman.dev>
2022-05-04 12:13:28 +0530

    use latest to build

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-curl -o mudkip.tgz -L https://github.com/barelyhuman/mudkip/releases/download/testing/linux-amd64.tgz
+curl -o mudkip.tgz -L https://github.com/barelyhuman/mudkip/releases/latest/download/linux-amd64.tgz
 tar -xvzf mudkip.tgz
 install linux-amd64/mudkip /usr/local/bin
 
