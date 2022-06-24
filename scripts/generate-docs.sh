6010fc1fe61e2885e324996e16cc0819426483bf
Reaper <ahoy@barelyhuman.dev>
2022-06-24 12:04:49 +0530

    add doc action

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p docs 
+curl -o mudkip.tgz -L https://github.com/barelyhuman/mudkip/releases/latest/download/linux-amd64.tgz
+tar -xvzf mudkip.tgz
+install linux-amd64/mudkip /usr/local/bin
+
+# hack for github pages till mudkip doesn't add baseURL functionality
+sed -i s#/#/mudkip/#g ./docs/_sidebar.md
+
+mudkip 
+
+
