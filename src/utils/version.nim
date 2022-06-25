7c8dc44e9ed27ba9b6ac6cbdca40e65f1bb702f0
Reaper <ahoy@barelyhuman.dev>
2022-06-25 19:59:21 +0530

    version bump

--- src/utils/version.nim
+++ src/utils/version.nim
@@ -2,4 +2,4 @@ proc currentVersion*():string =
     ## GENERATED CODE
     ## This file is generated using the generate-styles-nim.sh file with styles 
     ## from the original static/default-styles.css
-    return r"""v0.1.4-beta.0"""
+    return r"""v0.1.5"""
