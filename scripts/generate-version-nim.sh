da15263083ce70338599686d47024a4310b3a5b7
reaper <ahoy@barelyhuman.dev>
2022-11-27 02:21:40 -0600

    v0.1.6

--- scripts/generate-version-nim.sh
+++ scripts/generate-version-nim.sh
@@ -10,8 +10,7 @@ MINIFIED_OUTPUT=$(cat .commitlog.release)
 cat > src/utils/version.nim << EOF
 proc currentVersion*():string =
     ## GENERATED CODE
-    ## This file is generated using the generate-styles-nim.sh file with styles 
-    ## from the original static/default-styles.css
+    ## This file is generated using the generate-version-nim.sh
     return r"""$MINIFIED_OUTPUT"""
 EOF
 
--- src/utils/version.nim
+++ src/utils/version.nim
@@ -1,5 +1,4 @@
-proc currentVersion*(): string =
+proc currentVersion*():string =
     ## GENERATED CODE
-    ## This file is generated using the generate-styles-nim.sh file with styles
-    ## from the original static/default-styles.css
-    return r"""v0.1.5"""
+    ## This file is generated using the generate-version-nim.sh
+    return r"""v0.1.6"""
