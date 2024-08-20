75758a062b973d6d662e4bbdec3ade92ceb5dafc
reaper <ahoy@barelyhuman.dev>
2024-08-20 19:16:17 +0530

    ci: add in custom builder imge

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -2,17 +2,25 @@
 
 set -euxo pipefail
 
+build_linux_64=('nimble install -y \
+    ; nim c -d:release --opt:size --cpu:amd64 --os:linux -o:bin/linux-amd64/mudkip src/mudkip.nim && strip -s bin/linux-amd64/mudkip 
+')
+
 build_commands=(' nimble install -y \
-     ; nim c -d:release --opt:size -o:bin/linux-amd64/mudkip src/mudkip.nim && strip -s bin/linux-amd64/mudkip \
-     ; nim c -d:release --opt:size --cpu:arm64 --os:linux -o:bin/linux-arm64/mudkip src/mudkip.nim && strip -s bin/linux-arm64/mudkip  \
-     ; nim c -d:release --opt:size -d:mingw --cpu:i386 -o:bin/windows-386/mudkip.exe src/mudkip.nim \
-     ; nim c -d:release --opt:size -d:mingw --cpu:amd64 -o:bin/windows-amd64/mudkip.exe src/mudkip.nim
+      ; nim c -d:release --opt:size --cpu:arm64 --os:linux -o:bin/linux-arm64/mudkip src/mudkip.nim && strip -s bin/linux-arm64/mudkip  \
+      ; nim c -d:release --opt:size -d:mingw --cpu:i386 -o:bin/windows-386/mudkip.exe src/mudkip.nim \
+      ; nim c -d:release --opt:size -d:mingw --cpu:amd64 -o:bin/windows-amd64/mudkip.exe src/mudkip.nim
  ')
+
+docker run -it --rm --platform="linux/amd64" -v `pwd`:/app \
+    $(docker build --platform linux/amd64 . -q) \
+    /bin/bash -c "$build_linux_64"
 
  # run a docker container with osxcross and cross compile everything
  docker run -it --rm -v `pwd`:/app \
-    $(docker build . -q) \
+    $(docker build --platform linux/arm64 . -q) \
     /bin/bash -c "$build_commands"
+
 
 if [ "$(uname -s)" = "Darwin" ]
 then 
